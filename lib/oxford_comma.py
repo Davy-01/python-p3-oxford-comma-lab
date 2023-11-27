1737ebb5ba6983c6820418db42984638955c66a9
Davis <daviesnyakiba@gmail.com>
2023-11-27 05:07:55 +0300

    Completed Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items)==1:
+        stringify="".join(items)
+        return stringify
+    elif len(items)==2:
+        stringify=" and ".join(items)
+        return stringify
+    else:
+        stringify=", ".join(items[:-1]) + ", and " + items[-1]
+        return stringify
+    
